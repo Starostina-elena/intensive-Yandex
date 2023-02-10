cbcfbce9f5f09f8ef887003fb57dfa66f007968d
Лена <starostina.lena05@gmail.com>
2023-02-11 00:53:24 +0400

    изменения для flake

--- Django_3_2_15/Django_3_2_15/asgi.py
+++ Django_3_2_15/Django_3_2_15/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_3_2_15.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_3_2_15.settings')
 
 application = get_asgi_application()
