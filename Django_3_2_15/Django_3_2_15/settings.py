75eaf3ad24ae52b1628f4e1f96bbdc2429266742
Лена <starostina.lena05@gmail.com>
2023-02-10 20:35:31 +0400

    исправление issue 11

--- Django_3_2_15/Django_3_2_15/settings.py
+++ Django_3_2_15/Django_3_2_15/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.getenv('SECRET_KEY'))
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.getenv('DEBUG').lower() in ['y', 'yes', 'true', '1', 't']:
