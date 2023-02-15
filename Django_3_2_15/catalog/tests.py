473c1dcb014bc729c61654ca4f50e8c4db2fea80
Лена <starostina.lena05@gmail.com>
2023-02-15 14:49:32 +0400

    issue 13-16

--- Django_3_2_15/catalog/tests.py
+++ Django_3_2_15/catalog/tests.py
@@ -1,19 +1,23 @@
 from django.test import Client, TestCase
 
+from parameterized import parameterized
+
 
 class StaticUrlTests(TestCase):
     def test_catalog_endpoint(self):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
-        response = Client().get('/catalog/5')
-        self.assertEqual(response.status_code, 200)
-        response = Client().get('/catalog//5')
-        self.assertEqual(response.status_code, 200)
-        response = Client().get('/catalog/sfhbs/5')
-        self.assertEqual(response.status_code, 200)
-        response = Client().get('/catalog//-5')
-        self.assertEqual(response.status_code, 404)
-        response = Client().get('/catalog/sfhbs/-5')
-        self.assertEqual(response.status_code, 404)
-        response = Client().get('/catalog/-5')
-        self.assertEqual(response.status_code, 404)
+
+    @parameterized.expand(
+        [
+            ('/catalog/5', 200),
+            ('/catalog//5', 200),
+            ('/catalog/sfhbs/5', 200),
+            ('/catalog/-5', 404),
+            ('/catalog//-5', 404),
+            ('/catalog/sfhbs/-5', 404)
+        ]
+    )
+    def test_catalog_item_id_endpoint(self, url, code):
+        response = Client().get(url)
+        self.assertEqual(response.status_code, code)
