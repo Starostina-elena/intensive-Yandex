8afdf20500c84691682f4e7e1b8ccaa774a4ce42
Лена <starostina.lena05@gmail.com>
2023-02-16 00:24:21 +0400

    исправление issue по второй задаче (17-19)

--- Django_3_2_15/catalog/urls.py
+++ Django_3_2_15/catalog/urls.py
@@ -1,24 +1,14 @@
 from django.urls import path, re_path, register_converter
 
-from . import views
+from . import converter, views
 
 
-class PositiveNumber:
-    regex = '([0-9]+)'
-
-    def to_python(self, value):
-        return int(value)
-
-    def to_url(self, value):
-        return str(value)
-
-
-register_converter(PositiveNumber, 'pos_int')
+register_converter(converter.PositiveNumber, 'pos_int')
 
 
 urlpatterns = [
     path('', views.item_list),
     path('<int:item_id>', views.item_detail),
     path('<pos_int:item_id>', views.item_detail),
-    re_path(r'([^/]*)/(?P<item_id>[0-9]+)', views.item_detail)
+    re_path(r're/(?P<item_id>[0-9]+)', views.item_detail)
 ]
