9e0e379af31900556df09b339624068d0d438ed5
Лена <starostina.lena05@gmail.com>
2023-02-16 00:17:02 +0400

    исправление issue по первой задаче

--- Django_3_2_15/catalog/tests.py
+++ Django_3_2_15/catalog/tests.py
@@ -13,9 +13,17 @@ class StaticUrlTests(TestCase):
             ('/catalog/5', 200),
             ('/catalog//5', 200),
             ('/catalog/sfhbs/5', 200),
+            ('/catalog/10000', 200),
+            ('/catalog/ajcbdajd/chhjwebdf/1', 200),
+            ('/catalog//10500', 200),
             ('/catalog/-5', 404),
             ('/catalog//-5', 404),
-            ('/catalog/sfhbs/-5', 404)
+            ('/catalog/sfhbs/-5', 404),
+            ('/catalog/-3', 404),
+            ('/catalog//-100000', 404),
+            ('/catalog/hacbjskbckjdcndlcnjdskbckjdbc/-0', 404),
+            ('catalog/jasdb/sjkf/-5', 404),
+            ('/catalog/bbbd/ahcjk', 404)
         ]
     )
     def test_catalog_item_id_endpoint(self, url, code):
